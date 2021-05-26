ad679e370c6f9e8b2265081f94f574029932e70d
Rafael Zago <rafael.zago@br-mediagroup.com>
2021-05-26 10:13:43 -0300

    Create local save symbols test

--- src/data/usecases/local-save-symbols.spec.ts
+++ src/data/usecases/local-save-symbols.spec.ts
@@ -1,5 +1,52 @@
+class LocalSaveSymbols {
+  constructor(private readonly cacheStore: CacheStore) {}
+
+  async save(): Promise<void> {
+    this.cacheStore.delete('symbols');
+  }
+}
+
+interface CacheStore {
+  delete: (key: string) => void;
+}
+
+class CacheStoreSpy implements CacheStore {
+  deleteCallsCount = 0;
+  key: string;
+
+  delete(key: string): void {
+    this.deleteCallsCount++;
+    this.key = key;
+  }
+}
+
+type SutTypes = {
+  sut: LocalSaveSymbols;
+  cacheStore: CacheStoreSpy;
+};
+
+const makeSut = (): SutTypes => {
+  const cacheStore = new CacheStoreSpy();
+  const sut = new LocalSaveSymbols(cacheStore);
+  return { cacheStore, sut };
+};
+
 describe('LocalSaveSymbols', () => {
-  test('', () => {
-    expect(1).toBe(1);
+  test('Should not delete cache on sut.ini', () => {
+    const { cacheStore } = makeSut();
+    new LocalSaveSymbols(cacheStore);
+    expect(cacheStore.deleteCallsCount).toBe(0);
+  });
+
+  test('Should delete old cache on sut.save', async () => {
+    const { cacheStore, sut } = makeSut();
+    await sut.save();
+    expect(cacheStore.deleteCallsCount).toBe(1);
+  });
+
+  test('Should call delete with correct key', async () => {
+    const { cacheStore, sut } = makeSut();
+    await sut.save();
+    expect(cacheStore.key).toBe('symbols');
   });
 });
