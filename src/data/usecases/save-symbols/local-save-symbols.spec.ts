d737a2a8af658cbafd2d788b055041908e07364b
Rafael Zago <rafael.zago@br-mediagroup.com>
2021-05-27 10:47:06 -0300

    Save symbols timestamp

--- src/data/usecases/save-symbols/local-save-symbols.spec.ts
+++ src/data/usecases/save-symbols/local-save-symbols.spec.ts
@@ -6,16 +6,15 @@ type SutTypes = {
   cacheStore: CacheStoreSpy
 }
 
-const makeSut = (): SutTypes => {
+const makeSut = (timestamp = new Date()): SutTypes => {
   const cacheStore = new CacheStoreSpy()
-  const sut = new LocalSaveSymbols(cacheStore)
+  const sut = new LocalSaveSymbols(cacheStore, timestamp)
   return { cacheStore, sut }
 }
 
 describe('LocalSaveSymbols', () => {
   test('Should not delete or insert cache on sut.ini', () => {
     const { cacheStore } = makeSut()
-    new LocalSaveSymbols(cacheStore)
     expect(cacheStore.messages).toEqual([])
   })
 
@@ -28,6 +27,7 @@ describe('LocalSaveSymbols', () => {
   })
 
   test('Should insert new cache if delete succeeds', async () => {
+    const timestamp = new Date()
     const { cacheStore, sut } = makeSut()
     const symbols = mockSymbols()
     await sut.save(symbols)
@@ -37,7 +37,10 @@ describe('LocalSaveSymbols', () => {
     ])
     expect(cacheStore.deleteKey).toBe('symbols')
     expect(cacheStore.insertKey).toBe('symbols')
-    expect(cacheStore.insertValues).toEqual(symbols)
+    expect(cacheStore.insertValues).toEqual({
+      timestamp,
+      data: symbols,
+    })
   })
 
   test('Should throw error if insert fails', async () => {
--- src/data/usecases/save-symbols/local-save-symbols.ts
+++ src/data/usecases/save-symbols/local-save-symbols.ts
@@ -2,10 +2,16 @@ import { CacheStore } from '@/data/protocols/cache'
 import { SaveSymbols } from '@/domain/usecases'
 
 export class LocalSaveSymbols {
-  constructor(private readonly cacheStore: CacheStore) {}
+  constructor(
+    private readonly cacheStore: CacheStore,
+    private readonly timestamp: Date,
+  ) {}
 
   async save(symbols: Array<SaveSymbols.Params>): Promise<void> {
     this.cacheStore.delete('symbols')
-    this.cacheStore.insert('symbols', symbols)
+    this.cacheStore.insert('symbols', {
+      timestamp: this.timestamp,
+      data: symbols,
+    })
   }
 }
